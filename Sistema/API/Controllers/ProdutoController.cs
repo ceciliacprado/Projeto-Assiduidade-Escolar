e806c5a462c9692370e3d0dbb474d4a757a69951
Cecilia Camargo Prado <ceciliacprado1@gmail.com>
2025-05-03 17:40:22 -0300

    estrutura

---
+++ Sistema/API/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/produto")]
+public class ProdutoController : ControllerBase
+{
+    private readonly IProdutoRepository _produtoRepository;
+    public ProdutoController(IProdutoRepository produtoRepository)
+    {
+        _produtoRepository = produtoRepository;
+    }
+
+    [HttpPost("cadastrar")]
+    [Authorize(Roles = "administrador")]
+    public IActionResult Cadastrar([FromBody] Produto produto)
+    {
+        _produtoRepository.Cadastrar(produto);
+        return Created("", produto);
+    }
+
+    [HttpGet("listar")]
+    [Authorize]
+    public IActionResult Listar()
+    {        
+        return Ok(_produtoRepository.Listar());
+    }
+}
