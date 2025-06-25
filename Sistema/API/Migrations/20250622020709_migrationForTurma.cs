78ed33ccd8204754e2e40d361e5a26f59205edac
naoeoami <ami.guilherme05@gmail.com>
2025-06-25 19:34:34 -0300

    retirando migrations do gitignore

---
+++ Sistema/API/Migrations/20250622020709_migrationForTurma.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationForTurma : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Alunos_Email",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "Senha",
+                table: "Alunos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Especialidade",
+                table: "Professores",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CargaHoraria",
+                table: "Disciplinas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Codigo",
+                table: "Disciplinas",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProfessorId",
+                table: "Disciplinas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TurmaId",
+                table: "Disciplinas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TurmaId",
+                table: "Alunos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Turmas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ano = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Serie = table.Column<string>(type: "varchar(5)", maxLength: 5, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CriadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turmas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplinas_ProfessorId",
+                table: "Disciplinas",
+                column: "ProfessorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplinas_TurmaId",
+                table: "Disciplinas",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_TurmaId",
+                table: "Alunos",
+                column: "TurmaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Disciplinas_Professores_ProfessorId",
+                table: "Disciplinas",
+                column: "ProfessorId",
+                principalTable: "Professores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Disciplinas_Professores_ProfessorId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropTable(
+                name: "Turmas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Disciplinas_ProfessorId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Disciplinas_TurmaId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alunos_TurmaId",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "Especialidade",
+                table: "Professores");
+
+            migrationBuilder.DropColumn(
+                name: "CargaHoraria",
+                table: "Disciplinas");
+
+            migrationBuilder.DropColumn(
+                name: "Codigo",
+                table: "Disciplinas");
+
+            migrationBuilder.DropColumn(
+                name: "ProfessorId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropColumn(
+                name: "TurmaId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropColumn(
+                name: "TurmaId",
+                table: "Alunos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Alunos",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Senha",
+                table: "Alunos",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_Email",
+                table: "Alunos",
+                column: "Email",
+                unique: true);
+        }
+    }
+}
---
+++ Sistema/API/Migrations/20250624000202_MigrationReform.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationReform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alunos_Disciplinas_DisciplinaId",
+                table: "Alunos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alunos_DisciplinaId",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Professores");
+
+            migrationBuilder.DropColumn(
+                name: "DisciplinaId",
+                table: "Alunos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TurmaId",
+                table: "Disciplinas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TurmaId",
+                table: "Alunos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AlunoDisciplina",
+                columns: table => new
+                {
+                    AlunosId = table.Column<int>(type: "int", nullable: false),
+                    DisciplinasId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlunoDisciplina", x => new { x.AlunosId, x.DisciplinasId });
+                    table.ForeignKey(
+                        name: "FK_AlunoDisciplina_Alunos_AlunosId",
+                        column: x => x.AlunosId,
+                        principalTable: "Alunos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlunoDisciplina_Disciplinas_DisciplinasId",
+                        column: x => x.DisciplinasId,
+                        principalTable: "Disciplinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlunoDisciplina_DisciplinasId",
+                table: "AlunoDisciplina",
+                column: "DisciplinasId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas");
+
+            migrationBuilder.DropTable(
+                name: "AlunoDisciplina");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Professores",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TurmaId",
+                table: "Disciplinas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TurmaId",
+                table: "Alunos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisciplinaId",
+                table: "Alunos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_DisciplinaId",
+                table: "Alunos",
+                column: "DisciplinaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alunos_Disciplinas_DisciplinaId",
+                table: "Alunos",
+                column: "DisciplinaId",
+                principalTable: "Disciplinas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alunos_Turmas_TurmaId",
+                table: "Alunos",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Disciplinas_Turmas_TurmaId",
+                table: "Disciplinas",
+                column: "TurmaId",
+                principalTable: "Turmas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
